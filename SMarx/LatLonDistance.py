79b5ce71355da56414b85a3d8c1ac18d273be2c9
smarx2 <54850081+smarx2@users.noreply.github.com>
2020-05-27 09:44:26 -0500

    Update LatLonDistance.py

--- SMarx/LatLonDistance.py
+++ SMarx/LatLonDistance.py
@@ -1,9 +1,9 @@
 #written using the halversine formula (found on Google)
 import math
-Lat1=53.32055555555556
-Lat2=53.31861111111111
-Lon1=-1.7297222222222221
-Lon2=-1.6997222222222223
+Lat1=41.727489
+Lat2=41.364629
+Lon1=-99.442851
+Lon2=-99.530958
 Lat1=Lat1*math.pi/180
 Lat2=Lat2*math.pi/180
 Lon1=Lon1*math.pi/180
